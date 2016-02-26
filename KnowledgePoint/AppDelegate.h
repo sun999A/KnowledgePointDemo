8b7b81afea8bd727020c64cb0725cab7d52d0d45
Snail iOS <1170989734@qq.com>
2016-02-26 09:52:16 +0800

    Initial Commit

---
+++ KnowledgePoint/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KnowledgePoint
+//
+//  Created by Snail iOS on 16/2/26.
+//  Copyright © 2016年 Snail iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
